835527858f9532ff681bd937ba35c247d0a6233f
babji530035 <akbabji001@gmail.com>
2023-09-03 11:54:10 +0530

    PRACTICE PROGRAMMING
    PROGRAMS UNDER PRACTICE PROGRAMMING

---
+++ Pattern_1.java
@@ -0,0 +1,15 @@
+package PatternProgramming_level_2;
+
+public class Pattern_1 {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=5;i++) {
+			for(int j=5;j>=i;j--) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
---
+++ Pattern_12.java
@@ -0,0 +1,31 @@
+package PatternProgramming_level_2;
+
+public class Pattern_12 {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=5;i++) {
+			for(int j=1;j<=5;j++) {
+				System.out.print("-");
+			}
+			for(int k=5;k>=i;k--) {
+				System.out.print("*");
+				
+			}
+			System.out.println();
+		}
+		
+		
+		for(int i=2;i<=5;i++) {
+			for(int j=1;j<=5;j++) {
+				System.out.print("-");
+			}
+			for(int k=1;k<=i;k++) {
+				System.out.print("*");
+				
+			}
+			System.out.println();
+		}
+
+	}
+
+}
